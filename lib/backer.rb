09024b4c7a3fab6bd04fc46e4c15775d5042efcf
Reno Sorensen <renojames19@gmail.com>
2018-07-14 23:47:09 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -13,7 +13,7 @@ end
 
 def back_project(project)
   @backed_projects << project
-  project.add_backer(self)
+  project.add_backer(self) unless project.backers.include?(self)
 end
 
 
--- lib/project.rb
+++ lib/project.rb
@@ -13,6 +13,7 @@ end
 
 def add_backer(backer)
   @backers << backer
+  backer.back_project(self) unless backer.backed_projects.include?(self)
 end
 
 
