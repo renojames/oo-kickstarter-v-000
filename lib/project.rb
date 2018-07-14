47d3456ebbae9bffd5f4e93bc1d3d671ab7791ea
Reno Sorensen <renojames19@gmail.com>
2018-07-14 23:46:38 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -13,7 +13,7 @@ end
 
 def add_backer(backer)
   @backers << backer
-  backer.back_project(self) unless backer.backed_projects.includes?(self)
+  backer.back_project(self) unless backer.backed_projects.include?(self)
 end
 
 
